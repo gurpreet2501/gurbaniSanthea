94bdac60f290c9f783575e0c8691604f352bbaae
Gurpreet Singh <er.gurpreetsingh@live.com>
2017-05-27 14:31:43 +0530

    files changed

--- App/Models/AngLines.php
+++ App/Models/AngLines.php
@@ -9,4 +9,8 @@ class AngLines extends Model{
   protected $fillable = [];
   protected $table  = 'ang_lines';
 
+  function replacements(){
+  	return $this->hasMany(Replacements::class, 'line_id', 'id');
+  }
+
 }
--- application/controllers/Fetch.php
+++ application/controllers/Fetch.php
@@ -20,6 +20,26 @@ class Fetch extends CI_Controller{
     print_r(json_encode($angLines));
     
   }
+  
+  public function replacements(){
+   $r = $_POST;
+   
+   $angTbl =  Models\Ang::where('ang', $r['angNo'])->first();
+   
+   $angLines =  Models\AngLines::where('ang_id', $angTbl->ang)->with('replacements')->get();
+   
+
+   if(!count($angLines)){
+     $angLines = [];
+     return print_r(json_encode($angLines));
+   }
+    
+
+    $angLines = $angLines->toArray();
+    
+    print_r(json_encode($angLines));
+    
+  }
  	
 }
 
