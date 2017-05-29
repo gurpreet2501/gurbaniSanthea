31f9b434bac8ee664ed2facd1dcdb55d01f867c3
Gurpreet Singh <er.gurpreetsingh@live.com>
2017-05-29 20:48:25 +0530

    replacement code pushed

--- application/views/home.php
+++ application/views/home.php
@@ -7,7 +7,7 @@
  				<button class="btn-success show-replacements">Show Replacements</button>
  				<button class="btn-danger hide-replacements">Hide Replacements</button>
 					<div class="title clearfix"><h1 class="pull-right">ਅੰਗ <?=$ang_no?></h1></div>
-					<div class="border-frame">
+					<div class="border-frame img-thumbnail">
 						<div class="ang_display" data-ang="<?=$ang_no?>">
 						<?php foreach($angData->lines as $line): ?>
 							<span class="lines line_<?=$line->line_no?>"><?=$line->text?></span>
