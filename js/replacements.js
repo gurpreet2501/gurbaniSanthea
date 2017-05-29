eec2de4da14b8000a9942705eec36a0f29068c93
Gurpreet Singh <er.gurpreetsingh@live.com>
2017-05-29 12:43:09 +0530

    hello

--- js/replacements.js
+++ js/replacements.js
@@ -1,81 +1,18 @@
 jQuery(function(){
+	
+	var response = "";
 
 	var ang = $('.ang_display').attr('data-ang');
-	var data = {
-		getReplacements:function(ang){
-				
-				return $.ajax({
+	   
+	   $.ajax({
 				  method: "POST",
 				  url: location.origin+"/index.php/fetch/replacements",
 				  data: { angNo: ang }
-				})
-				  .done(function( msg ) {
-				    var obj = JSON.parse(msg);
-				    data.changeColor(obj);
+				}).done(function(msg) {
+				  	response = JSON.parse(msg);
+				  	console.log(response);
 
-				    $('.red-color').hover(
-						  function() {
-						    data.replaceText(obj);
-						  }, function() {
-						  	
-						    data.revertOriginal(obj);
-						  }
-						);
-
-				    $('.red-color').hover(function(){
-				    	
-				    });
 			  });
-				  
-		},
-		replaceText:function(obj){
-			$.each(obj, function(key, value){
-				// console.log(value);
-				var nkey = ++key;
-				
-				if(value.line_no == 2){
-					
-					$.each(value.replacements, function(ky,v){
-						var str = $('.line_2').text();
-						str = str.replace(v.original_word, '<span class="red-color">'+v.replacement+'</span>');
-						$('.line_2').html(str);
-					})
-				}
-			})
-		},
-
-		revertOriginal:function(obj){
-			$.each(obj, function(key, value){
-				// console.log(value);
-				var nkey = ++key;
-				
-				if(value.line_no == 2){
-					$.each(value.replacements, function(ky,v){
-						var str = $('.line_2').text();
-						str = str.replace(v.replacement, v.original_word);
-						$('.line_2').html(str);
-					})
-				}
-			})
-		},
-
-		changeColor:function(obj){
-			$.each(obj, function(key, value){
-				// console.log(value);
-				var nkey = ++key;
-				
-				if(value.line_no == 2){
-					$.each(value.replacements, function(ky,v){
-						var str = $('.line_2').text();
-						str = str.replace(v.original_word, '<span class="red-color">'+v.original_word+'</span>');
-						$('.line_2').html(str);
-					})
-				}
-			})
-		},
-
-	};	
-
-	data.getReplacements(ang);
 	
+		
 })
