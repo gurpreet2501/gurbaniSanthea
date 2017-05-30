62c1b9449893d47a2a638f5fed39960413422937
Gurpreet Singh <er.gurpreetsingh@live.com>
2017-05-30 16:15:35 +0530

    files deleted

--- application/views/pages/childline.php
+++
@@ -1,1 +0,0 @@
-<img src="images/child.jpg" class="img-fit-container" align="center"/>
--- application/views/pages/maps.php
+++
@@ -1,4 +0,0 @@
-<hr/>
-<img src="<?=base_url('images/tehsil.jpg')?>" class="img-fit-container" />
-<hr/>
-<img src="<?=base_url('images/district.jpg')?>" class="img-fit-container" />
